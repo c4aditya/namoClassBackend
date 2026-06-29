34807670182a1b2ad753d7357c91049e8d62ea45
Aditya Singh <singhas1418@gmail.com>
2026-06-29 23:19:27 +0530

    adding cros

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+     origin: [
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://aviation-courses.com",
+        "https://www.aviation-courses.com"
+    ],
     credentials: true
 }));
 
